75f0bc56f5c8784c0b5e7f4127e92ff533024b0b
alexis <atroyvb@gmail.com>
2024-04-02 18:10:44 -0400

    Small modifications.

--- npi_grants/sql/create_tables.sql
+++ npi_grants/sql/create_tables.sql
@@ -1,19 +1,19 @@
-CREATE TABLE IF NOT EXISTS grantee ( 
-    id INTEGER PRIMARY KEY NOT NULL, 
-    application_id INTEGER NOT NULL, 
+CREATE TABLE IF NOT EXISTS grantee (
+    id INTEGER PRIMARY KEY NOT NULL,
+    application_id INTEGER NOT NULL,
     budget_start DATETIME,
-    grant_type VARCHAR(3) NOT NULL, 
-    total_cost FLOAT, 
-    forename VARCHAR(100)
+    grant_type VARCHAR(3) NOT NULL,
+    total_cost FLOAT,
+    is_contact BOOLEAN NOT NULL,
+    forename VARCHAR(100),
     last_name VARCHAR(100) NOT NULL,
     organization VARCHAR(100),
     city VARCHAR(100),
     state VARCHAR(100),
-    country   VARCHAR(100)
+    country VARCHAR(100)
 );
 
-            
-CREATE TABLE IF NOT EXISTS provider_npi(
+CREATE TABLE IF NOT EXISTS provider (
     id INTEGER PRIMARY KEY NOT NULL,
     npi INT NOT NULL,
     taxonomy_code VARCHAR(25),
@@ -26,11 +26,23 @@ CREATE TABLE IF NOT EXISTS provider_npi(
     country VARCHAR(100)
 );
 
--- the bridge table between grantees and providers
+-- The bridge table between grantees and providers
 CREATE TABLE IF NOT EXISTS grantee_provider (
-    grantee_id INT NOT NULL, 
+    grantee_id INT NOT NULL,
     provider_id INT NOT NULL,
     FOREIGN KEY(grantee_id) REFERENCES grantee(id),
     FOREIGN KEY(provider_id) REFERENCES provider(id),
     UNIQUE(grantee_id, provider_id)
-)
+);
+
+
+
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
